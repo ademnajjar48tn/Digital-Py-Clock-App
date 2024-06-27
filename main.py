bc0f7b3e5b8ebea29f1ab1eaaae6d9e699a94087
ademnajjar48 <cctestingportugal@gmail.com>
2024-06-27 23:46:17 +0100

    Import time

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from datetime import datetime
 from tkinter import Tk
 from tkinter import Label
+import time
 
 window = Tk()
 
 master.title("Digital Clock")
-
+ 
 time = Label(master, font=("Helvetica", 90), bg="black", fg="white")
 
 time.pack()
